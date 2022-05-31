2460212ff93d373fcd6bc99650ed104a74b942a2
F1shS0up <alkanjebest@gmail.com>
2022-05-31 16:04:11 +0200

    lol

---
+++ platformer/platformer/bin/Debug/netcoreapp3.1/biom2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.5" name="biom2" tilewidth="16" tileheight="16" tilecount="36" columns="6">
+ <image source="level_1set.png" width="96" height="96"/>
+</tileset>
